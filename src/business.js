eac45d0f248589743c922bc31f4b9dfe62a8d2dd
stewiebeamen <jmstew8@gmail.com>
2024-01-25 14:31:54 -0500

    Create Business react component

---
+++ src/business.js
@@ -0,0 +1,11 @@
+function Business() {
+    this.image = "";
+    this.name = "";
+    this.address = "";
+    this.city = "";
+    this.state = "";
+    this.zipcode = "";
+    this.category = "";
+    this.rating = "";
+    this.reviewCount = "";
+}
