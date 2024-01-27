d5a026ae89e2a38288b062e678b84e78b19827aa
jmstew3 <33553552+jmstew3@users.noreply.github.com>
2024-01-27 08:57:43 -0500

    Update javascript in business.js

--- src/business.js
+++ src/business.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-function Business() {
-    const image = "https://content.codecademy.com/programs/react/ravenous/pizza.jpg";
-    const name = "MarginOtto Pizzeria";
-    const address = "1010 Paddington Way";
-    const city = "Flavortown";
-    const state = "NY";
-    const zipcode = "10101";
-    const category = "Italian";
-    const rating = "4.5";
-    const reviewCount = "90";
+const businessData = {
+    image: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+    name: "MarginOtto Pizzeria",
+    address: "1010 Paddington Way",
+    city: "Flavortown",
+    state: "NY",
+    zipcode: "10101",
+    category: "Italian",
+    rating: "4.5",
+    reviewCount: "90"
+};
 
+function Business() {
     return (
-        <div>
-            {/* Your JSX code here */}
-        </div>
+      
     );
 }
 
-export default Business;
+export default Business
