d7bccaf4b6b96a63438d9dcaf60c053c26a7935f
jmstew3 <jmstew8@gmail.com>
2024-01-27 12:06:28 -0500

    Update business.js to use JSX .map() method

--- src/business.js
+++ src/business.js
@@ -1,30 +1,11 @@
 import React from 'react';
 
-const businessData = {
-    image: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-    name: "MarginOtto Pizzeria",
-    address: "1010 Paddington Way",
-    city: "Flavortown",
-    state: "NY",
-    zipcode: "10101",
-    category: "Italian",
-    rating: "4.5",
-    reviewCount: "90"
-};
+const businessData = ['image', 'name', 'address', 'city', 'state', 'zipcode', 'category', 'rating', 'reviewCount'];
+const listItems = businessData.map(data => <li>{data}</li>);
 
 function Business() {
     return (
-        <div>
-            image={businessData.image}
-            name={businessData.name}
-            address={businessData.address}
-            city={businessData.city}
-            state={businessData.state}
-            zipcode={businessData.zipcode}
-            category={businessData.category}
-            rating={businessData.rating}
-            reviewCount={businessData.reviewCount}
-        </div>
+        <li>{listItems}</li>
     );
 }
 
