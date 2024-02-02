65238ace0b75991c3be0a4f2c23e1c9aee53b3b2
stewiebeamen <jmstew8@gmail.com>
2024-02-02 11:55:26 -0500

    Add React import and rearrange imports in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-
+import React from 'react';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
 
 
 const business = {
